c7f023f115f596e9e6e408e3ecaf046b71aa26c9
olegmusin <superfilch@yandex.by>
2018-03-12 12:37:18 +0300

    not found page fix

--- app/app.js
+++ app/app.js
@@ -3,7 +3,6 @@ import React from "react";
 import Nav from "./components/nav/Nav";
 import SideBar from "./components/utilities/sideBar/SideBar";
 import Loader from "./components/utilities/preLoader/Loader";
-import NotFoundPage from "./components/utilities/notFoundPage/NotFoundPage";
 import * as routes from "../constants/routes";
 import SignUpPage from "./components/auth/SignUp";
 import SignInForm from "./components/auth/SignIn";
@@ -14,6 +13,7 @@ import TimeTrackerAddContainer from "./containers/TimeTrackerAddContainer";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignInContainer from "./containers/SignInContainer";
+import NotFoundCountainer from "./containers/NotFoundCountainer";
 
 require("./app.scss");
 
@@ -28,7 +28,7 @@ const App = () => (
         <Route exact path={routes.ADD} component={TimeTrackerAddContainer} />
         <Route exact path={routes.SIGN_IN} component={SignInContainer} />
         <Route exact path={routes.SIGN_UP} component={SignUpPage} />
-        <Route path='*' exact={true} component={NotFoundPage} />
+        <Route path='*' exact={true} component={NotFoundCountainer} />
       </Switch>
       <SnackBar />
     </div>
